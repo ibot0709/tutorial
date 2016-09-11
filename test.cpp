883135f5b356075953d61b81f9a5ef71cffa38b2
ibot0709 <ibot0709@gmail.com>
2016-09-11 17:38:28 +0900

    add List code

--- test.cpp
+++ test.cpp
@@ -3,13 +3,56 @@
 
 using namespace std;
 
+template <typename T>
+class List{
+private:
+	struct Node{
+		T data;
+		Node* next;
+		Node* prev;
+		
+		Node(T data, Node* next, Node* prev){
+			this->data = data;
+			this->next = next;
+			this->prev = prev;
+		}
+	};
+	Node* head;
+	Node* curr;
+	
+public:
+	List(){};
+	void add(T data){
+		Node* newNode = new Node(data, NULL, NULL);
+		
+		if(head == NULL){
+			head = newNode;
+		}
+		curr = newNode;
+	}
+	T next(){
+		curr = curr->next;
+		return curr->data;
+	}
+	bool hasNext(){
+		if(curr->next == NULL){
+			return false;
+		}
+		return true;
+	}
+	T get(){ return curr->data }
+};
+
 int main(){
 	
-	vector<int> v = {1, 2, 3};
-
-	for (int i : v) {
-		cout << i << endl;
+	List<int> lst;
+	lst.add(1);
+	lst.add(2);
+	lst.add(3);
+	
+	while(lst.hasNext){
+		cout << lst.get() << endl;
 	}
-
+	
 	return 0;
 }
